4923adf7412669916c7561f7c49ba09b2fa3d2f6
alina <a.pirogova95@mail.ru>
2024-05-26 01:12:06 +0300

    ci/cd homework

--- src/test/java/com/example/teamcity/ui/SetupTest.java
+++ src/test/java/com/example/teamcity/ui/SetupTest.java
@@ -3,6 +3,7 @@ package com.example.teamcity.ui;
 import com.codeborne.selenide.Condition;
 import com.example.teamcity.ui.pages.setup.AgentsPage;
 import com.example.teamcity.ui.pages.setup.AgentsUnauthPage;
+import com.example.teamcity.ui.pages.setup.LoginSuperuserPage;
 import com.example.teamcity.ui.pages.setup.StartUpPage;
 import org.testng.annotations.Test;
 
@@ -17,6 +18,9 @@ public class SetupTest extends BaseUiTest {
 
     @Test
     public void setupTeamCityAgentTest() {
+        new LoginSuperuserPage()
+                .open()
+                .login();
         new AgentsUnauthPage()
                 .open()
                 .authorizeAgent();
